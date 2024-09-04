74b3237dd97880bfa76e2d08793bc22ff8ab7b97
Victor Popkov <victor@popkov.me>
2024-09-04 16:37:34 +0300

    Make some improvements in bump-imagemagick.sh
    - Add optional output redirection in print_bold()
    - Remove redundant shift calls and streamline argument handling
    - Replace print_bold_red() with print_bold_color()

--- bin/bump-imagemagick.sh
+++ bin/bump-imagemagick.sh
@@ -25,25 +25,30 @@ usage() {
 
 print_bold() {
   local value="$1"
-  if [ "$DISABLE_COLORS" = '1' ] || ! [ -t 0 ]; then
-    printf '%s' "$value"
+  local output="${3:-1}"
+
+  if [ "$DISABLE_COLORS" = '1' ] || ! [ -t 1 ]; then
+    printf '%s' "$value" >&"$output"
   else
-    printf "$(tput bold)%s$(tput sgr0)" "$value"
+    printf "$(tput bold)%s$(tput sgr0)" "$value" >&"$output"
   fi
 }
 
-print_bold_red() {
-  local value="$1"
-  if [ "$DISABLE_COLORS" = '1' ] || ! [ -t 0 ]; then
-    printf '%s' "$value"
+print_bold_color() {
+  local color="$1"
+  local value="$2"
+  local output="${3:-1}"
+
+  if [ "$DISABLE_COLORS" = '1' ] || ! [ -t 1 ]; then
+    printf '%s' "$value" >&"$output"
   else
-    printf "$(tput bold)$(tput setaf 1)%s$(tput sgr0)" "$value"
+    printf "$(tput bold)$(tput setaf "$color")%s$(tput sgr0)" "$value" >&"$output"
   fi
 }
 
 print_error() {
-  print_bold_red "error: $1"
-  echo ''
+  print_bold_color 1 "error: $1" 2
+  echo '' >&2
 }
 
 summary() {
